e2baf214fbc7afd97cac8a9e3a9da00e140c2107
Harshad Marathe <harshadmarathe8@gmail.com>
2024-04-13 22:28:37 +0530

    Dockerfile modification in myapp-backend, backend web services deployed, url of cloud hosted webservices modified in frontend

--- book-frontend/src/common/constant.js
+++ book-frontend/src/common/constant.js
@@ -1,1 +1,2 @@
-export const BASE_API_URL = "http://localhost:8081";
+// export const BASE_API_URL = "http://localhost:8081";
+export const BASE_API_URL = "https://e-commerce-backend-mipc.onrender.com";
